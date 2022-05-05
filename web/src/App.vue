7682c18ff92fcf11a9a089274779d877ea262c7d
yangyuxiao <yangyuxiao@cloudwalk.com>
2022-05-05 18:06:13 +0800

    页脚

--- web/src/App.vue
+++ web/src/App.vue
@@ -72,6 +72,9 @@
         </a-layout-content>
       </a-layout>
     </a-layout>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
   </a-layout>
 </template>
 
