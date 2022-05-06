4fcc3f3e1bd79c9d2f585773013793fca29a5394
yangyuxiao <yangyuxiao@cloudwalk.com>
2022-05-06 13:42:18 +0800

    制作 the-header.vue 和 the-footer.vue 组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    电子书管理系统 ©2022 Created by
+  </a-layout-footer>
+
+</template>
+
+<script>
+export default {
+  name: "the-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
