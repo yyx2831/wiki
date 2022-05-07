bb70e1a2ea59d90f0e38570566f2af30b429aee4
yangyuxiao <yangyuxiao@cloudwalk.com>
2022-05-07 13:32:22 +0800

    安装 @ant-design/icons-vue 并引入a-list组件 npm i @ant-design/icons-vue-save 并且全局引入图标库

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).use(antd).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(antd).mount('#app')
+
+// 全局使用图标
+const icons: any = Icons;
+for (const key in icons) {
+    app.component(key, icons[key]);
+}
