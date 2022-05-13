9b8710ebf9a0770b7e815d3738c2503cc0c2dffe
yangyuxiao <yangyuxiao@cloudwalk.com>
2022-05-13 15:40:21 +0800

    7-3 完成分页管理基本的增删改查功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -10,7 +10,10 @@
         <router-link to="/">首页</router-link>
       </a-menu-item>
       <a-menu-item key="/admin/ebook">
-        <router-link to="/admin/ebook">电子书</router-link>
+        <router-link to="/admin/ebook">电子书管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
@@ -26,5 +29,18 @@ export default {
 </script>
 
 <style scoped>
-
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
+.login-menu {
+  float: right;
+  color: white;
+  padding-left: 10px;
+}
 </style>
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
+import AdminCategory from '../views/admin/admin-category.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -20,6 +21,14 @@ const routes: Array<RouteRecordRaw> = [
     path: '/admin/ebook',
     name: 'AdminEbook',
     component: AdminEbook
+  },
+  {
+    path: '/admin/category',
+    name: 'AdminCategory',
+    component: AdminCategory,
+    meta: {
+      loginRequire: true
+    }
   }
 ]
 
